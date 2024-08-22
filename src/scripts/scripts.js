ef598ee3c37fd42a1b7310db97ced87f8deb1acc
Ksylika <ksutalika@gmail.com>
2024-08-22 17:28:16 +0300

    Global commit v2
    JS and HTML update

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,231 @@
+// Функция для создания и добавления карточки проекта
+class ProjectManager {
+  constructor(projectCard, frontendSkills, backendSkills, subSkills, aboutMe) {
+    this.projectCardContainer = document.querySelector(projectCard);
+    this.frontendContainer = document.getElementById(frontendSkills);
+    this.backendContainer = document.getElementById(backendSkills); 
+    this.subSkillsContainer = document.querySelector(subSkills);
+    this.aboutMeContainer = document.querySelector(aboutMe);
+  }
+  
+  createProjectCard(project) {
+    const cardHTML = `
+            <article class="projectCard">
+                <img src="${project.imgSrc}" alt="Проект" class="projectCard__image" width="500px" height="250px" loading="lazy">
+                <h3 class="projectCard__title">
+                    ${project.title}
+                </h3>
+                <div class="projectCard__line"></div>
+                <p class="projectCard__description">
+                    ${project.description}
+                </p>
+                <a href="${project.githubLink}" alt="Ссылка на GitHub" class="button projectCard__button" target="_blank">
+                    GitHub
+                </a>
+            </article>
+        `;
+    this.projectCardContainer.insertAdjacentHTML('beforeend', cardHTML);
+  }
+  
+  createFrontendList(item) {
+    const frontendListHTML = `
+      <li class="technologies__item">
+        <div class="technologies__item-container">
+            <p class="technologies__item-name">${item.title}</p>
+            <span class="technologies__item-level">${item.level}</span>
+            </div>
+              <div class="technologies__item-progress">
+              <div class="technologies__item-progress-line" data-level="${item.levelScore}"></div>
+            </div>
+        </li>
+    `
+    this.frontendContainer.insertAdjacentHTML('beforeend', frontendListHTML);
+  }
+  
+  createBackendList(item) {
+    const backendListHTML = `
+      <li class="technologies__item">
+        <div class="technologies__item-container">
+            <p class="technologies__item-name">${item.title}</p>
+            <span class="technologies__item-level">${item.level}</span>
+            </div>
+              <div class="technologies__item-progress">
+              <div class="technologies__item-progress-line" data-level="${item.levelScore}"></div>
+            </div>
+        </li>
+    `
+    this.backendContainer.insertAdjacentHTML('beforeend', backendListHTML);
+  }
+  
+  createSubSkills(item) {
+    const subSlillListHTML = `
+    <li class="technologies__item-name technologies__item-substack">${item}</li>
+    `
+    this.subSkillsContainer.insertAdjacentHTML('beforeend', subSlillListHTML);
+  }
+  
+  createAboutMeCard(item) {
+    const subSlillListHTML = `
+    <article class="aboutMe__card">
+      <span class="aboutMe__card-year">
+          ${item.year}
+      </span>
+      <div class="aboutMe__card-timeLine">
+        <img src="./src/image/icons/dot.svg" class="aboutMe__card-timeLine-dot" width="32px" height="32px">
+        <div class="aboutMe__card-timeLine-line"></div>
+      </div>
+      <p class="aboutMe__card-description">${item.description}.</p>
+    </article>
+    `
+    this.aboutMeContainer.insertAdjacentHTML('beforeend', subSlillListHTML);
+  }
+  
+  createMultipleProjectCards(items) {
+    items.forEach(item => this.createProjectCard(item));
+  }
+  
+  createMultipleFrontendList(items) {
+    items.forEach(item => this.createFrontendList(item));
+  }
+  
+  createMultipleBackendList(items) {
+    items.forEach(item => this.createBackendList(item));
+  }
+  
+  createMultipleSubSkills(items) {
+    items.forEach(item => this.createSubSkills(item));
+  }
+  
+  createMultipleAboutMeCard(items) {
+    items.forEach(item => this.createAboutMeCard(item));
+  }
+  
+}
+
+
+const projectManagerPacket = new ProjectManager(
+  '.project__wrapper',
+  'frontend',
+  'backend',
+  '.technologies__list-substack',
+  '.aboutMe__wrapper'
+)
+
+const projects = [
+  {
+    imgSrc: './src/image/kseniaPorfolio.jpg',
+    title: 'Личный сайт портфолио',
+    description: 'Проект реализован с использованием HTML, CSS, JavaScript и SASS. Применены методология БЭМ и адаптивная верстка, что обеспечивает структурированность кода и удобство просмотра.',
+    githubLink: 'https://github.com/Ksylika/KseniaPortfolio'
+  },
+  {
+    imgSrc: './src/image/comingSoonPlaceholder2.jpg',
+    title: 'Проект в разработке',
+    description: 'В этом месте вскоре появится новый проект, который станет отражением моего стремления к совершенству и инновациям. Я намерена показать,  как можно сочетать творчество и технологии.',
+    githubLink: '#'
+  },
+  {
+    imgSrc: './src/image/comingSoonPlaceholder2.jpg',
+    title: 'Проект в разработке',
+    description: 'В этом месте вскоре появится новый проект, который станет отражением моего стремления к совершенству и инновациям. Я намерена показать,  как можно сочетать творчество и технологии.',
+    githubLink: '#'
+  },
+  {
+    imgSrc: './src/image/comingSoonPlaceholder2.jpg',
+    title: 'Проект в разработке',
+    description: 'В этом месте вскоре появится новый проект, который станет отражением моего стремления к совершенству и инновациям. Я намерена показать,  как можно сочетать творчество и технологии.',
+    githubLink: '#'
+  },
+];
+
+const frontendSkills = [
+  {
+    title: 'HTML',
+    level: 'Продвинутый',
+    levelScore: '80',
+  },
+  {
+    title: 'CSS, SASS',
+    level: 'Продвинутый',
+    levelScore: '80',
+  },
+  {
+    title: 'JavaScript',
+    level: 'Средний',
+    levelScore: '60',
+  },
+  {
+    title: 'Vue',
+    level: 'Начальный',
+    levelScore: '30',
+  },
+  {
+    title: 'React',
+    level: 'Начальный',
+    levelScore: '30',
+  },
+  
+  
+]
+
+const backendSkills = [
+  {
+    title: 'Python',
+    level: 'Продвинутый',
+    levelScore: '80',
+  },
+  {
+    title: 'PHP',
+    level: 'Средний',
+    levelScore: '60',
+  },
+  {
+    title: 'MySQL',
+    level: 'Средний',
+    levelScore: '60',
+  },
+  {
+    title: 'Django',
+    level: 'Начальный',
+    levelScore: '30',
+  },
+]
+
+const subSkills = [
+  'Git',
+  'БЭМ',
+  'VS Code',
+  'Адаптивная верстка',
+  'Figma'
+]
+
+const aboutMe = [
+  {
+    year: '2021',
+    description: 'Я завершила обучение в колледже "МГОК" по специальности системного администрирования.'
+  },
+  {
+    year: '2021 - н.в.',
+    description: `
+    На нынешней работе, прошла путь от мл. Системного администратора до Старшей группы Системного администрирования. 
+    Выполняя различные задачи от тех. поддержки до написания скриптов и веб. разработки.`
+  },
+  {
+    year: '2023 - н.в.',
+    description: `Я учусь в университете Витте, на прикладной информатики по специализации Искусственный интеллект и анализ данных.`
+  }
+  
+]
+
+
+
+projectManagerPacket.createMultipleProjectCards(projects);
+projectManagerPacket.createMultipleFrontendList(frontendSkills);
+projectManagerPacket.createMultipleBackendList(backendSkills);
+projectManagerPacket.createMultipleSubSkills(subSkills);
+projectManagerPacket.createMultipleAboutMeCard(aboutMe);
+
+
 const burgerbutton = () => {
   const burgerButton = document.querySelector('.burger');
   const modalNavigation = document.querySelector('.navigation');
